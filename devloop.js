8ff3a63d9526010190c675d4b5ed6220ad5140db
Clement Delafargue <clement@delafargue.name>
2017-06-01 15:08:45 +0200

    Make devloop platform-independent

--- devloop.js
+++ devloop.js
@@ -8,7 +8,7 @@ let compile = run({
 let server = runServer({
   httpPort,
   env: { "PORT": httpPort },
-  sh: `./.stack-work/install/x86_64-osx/lts-8.15/8.0.2/bin/cestpasnous`
+  sh: `./.stack-work/install/*/*/*/bin/cestpasnous`
 }).dependsOn(compile)
 
 proxy(server, 8080).dependsOn(compile)
