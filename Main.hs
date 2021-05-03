cc936c212ae7f6621477fb84d7db58bb532a3011
Clement Delafargue <clement@delafargue.name>
2021-05-03 11:05:46 +0200

    Bump resolver + hlint fixes

--- Main.hs
+++ Main.hs
@@ -5,7 +5,6 @@ module Main where
 
 import           Control.Monad.IO.Class  (liftIO)
 import           Data.ByteString.Builder (toLazyByteString)
-import           Data.Monoid             ((<>))
 import           Data.Text               (Text, toLower, unpack)
 import           Data.Text.Lazy.Encoding (decodeUtf8)
 import           Lucid                   (Html, renderText, toHtml)
@@ -18,12 +17,10 @@ main :: IO ()
 main = do
   port <- lookupEnv "PORT"
   scotty (maybe 8080 read port) $ do
-    get "/" $ do
+    get "/" $
       html $ renderText form
-    get "/style.css" $ do
+    get "/style.css" $
       file "style.css"
-    get "/logo.png" $ do
-      file "logo.png"
     post "/" $ do
       a <- param "a"
       b <- param "b"
@@ -38,29 +35,26 @@ isReserved :: Text -> Bool
 isReserved = (`elem` ["cyril hanouna", "hanouna", "tpmp", "touche pas à mon poste", "manuel valls", "valls"]) . toLower
 
 render :: Text -> Text -> Html ()
-render a b =
-  if isReserved a then
-    reserved a
-  else if isReserved b then
-    reserved b
-  else notme a b
+render a b
+  | isReserved a = reserved a
+  | isReserved b = reserved b
+  | otherwise    = notme a b
 
 layout :: Html () -> Html ()
 layout content = doctypehtml_ $ do
-  head_ $ do
+  head_ $
     link_ [type_ "text/css", rel_ "stylesheet", href_ "/style.css"]
-  body_ $ do
-    content
+  body_ content
 
 notme :: Text -> Text -> Html ()
-notme a b = layout $ do
+notme a b = layout $
   h1_ $ do
     b_ $ toHtml a <> br_ []
     " n'a " <> i_ "rien" <> " à voir" <> br_ [] <> "avec "
-    (b_ $ toHtml b) <> "."
+    b_ (toHtml b) <> "."
 
 reserved :: Text -> Html ()
-reserved kw = layout $ do
+reserved kw = layout $
   h1_ $ do
     "Se dédouaner de "
     b_ $ toHtml kw <> br_ []
@@ -69,7 +63,7 @@ reserved kw = layout $ do
 
 
 form :: Html ()
-form = layout $ do
+form = layout $
   form_ [action_ "/", method_ "post"] $ do
     input_ [type_ "text", name_ "a", placeholder_ "@clementd"] <> br_ []
     input_ [type_ "text", name_ "b", placeholder_ "ce site inutile"] <> br_ []
